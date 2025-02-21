9a288a10354e54b4686b99aa31ac98df86f75e93
YohanMenager <yohan.menager0600@gmail.com>
2025-02-21 13:26:08 +0100

    ajout de la skybox au 3e jeu

--- front/sections/Jeu3/Jeu3.js
+++ front/sections/Jeu3/Jeu3.js
@@ -22,6 +22,8 @@ export function initBabylon() {
         let scene = new BABYLON.Scene(engine);
         scene.clearColor = new BABYLON.Color3.White();
 
+
+        /*-----------------------------------------------------------------------------création des objets-----------------------------------------------------------------------------*/
         let box = BABYLON.Mesh.CreateBox("Box", 4.0, scene);
         let box2 = BABYLON.Mesh.CreateBox("Box2", 4.0, scene);
         box.position.y = 2;
@@ -33,7 +35,7 @@ export function initBabylon() {
         
         let material2 = new BABYLON.StandardMaterial("material2", scene);
         material2.diffuseColor = new BABYLON.Color3(0.2, 0, 0);
-        material2.emissiveColor = new BABYLON.Color3(0.1, 0.1, 0.1);
+        material2.emissiveColor = new BABYLON.Color3(0, 0, 0);
         material2.diffuseTexture = new BABYLON.Texture("/assets/textures/texture1.jpg", scene);
         material2.bumpTexture = new BABYLON.Texture("/assets/normalMaps/normalMap1.jpg", scene);
         box.material = material2;
@@ -42,21 +44,39 @@ export function initBabylon() {
         // let textureNuage = new BABYLON.Texture("/assets/textures/nuages.jpg", scene);
         // textureNuage.bumpTexture = new BABYLON.Texture("/assets/normalMaps/nuages.jpg", scene);
 
+        /*-----------------------------------------------------------------------------sol-----------------------------------------------------------------------------*/
+
         let materiauSol = new BABYLON.StandardMaterial("materiauSol", scene);
 
         materiauSol.bumpTexture = new BABYLON.Texture("/assets/normalMaps/nuages2.jpg", scene);
         materiauSol.diffuseColor = new BABYLON.Color3(1, 1, 1);
-        materiauSol.emissiveColor = new BABYLON.Color3(0.5, 0.5, 0.5);
+        // materiauSol.emissiveColor = new BABYLON.Color3(0.5, 0.5, 0.5);
 
         materiauSol.bumpTexture.uScale = 10; // Nombre de répétitions en largeur
         materiauSol.bumpTexture.vScale = 10; // Nombre de répétitions en hauteur
-        materiauSol.bumpTexture.level = 5; // Augmente l'effet de relief
+        materiauSol.bumpTexture.level = 1; // Augmente l'effet de relief
         
 
         let sol = BABYLON.MeshBuilder.CreateGround("sol", { width: 500, height: 500 }, scene);
         sol.material = materiauSol;
 
 
+        /*-----------------------------------------------------------------------------skybox-----------------------------------------------------------------------------*/
+
+        const skybox = BABYLON.MeshBuilder.CreateBox("skyBox", { size: 1000.0 }, scene);
+        const skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+        skyboxMaterial.backFaceCulling = false;
+        skyboxMaterial.disableLighting = true;
+        skybox.material = skyboxMaterial;
+        skybox.infiniteDistance = true;
+
+        skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("/assets/textures/skybox/ciel", scene);
+        skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+
+        // skyboxMaterial.emissiveColor = new BABYLON.Color3(0.5, 0.5, 0.5); 
+
+
+        /*-----------------------------------------------------------------------------caméra-----------------------------------------------------------------------------*/
 
         let camera = new BABYLON.FollowCamera("followCamera", BABYLON.Vector3.Zero(), scene);
         camera.lockedTarget = box;
@@ -83,13 +103,16 @@ export function initBabylon() {
             if (inputMap["d"]) box.position.x += speed;
         });
 
-        let light = new BABYLON.PointLight("light1", new BABYLON.Vector3(0, 10, 0), scene);
-        light.diffuse = new BABYLON.Color3(0.9, 0.9, 0.9);
+
+        /*-----------------------------------------------------------------------------lumière-----------------------------------------------------------------------------*/
+        let lumiere = new BABYLON.HemisphericLight("HemiLight", new BABYLON.Vector3(0, 1, 0), scene);
+        // lumiere.diffuse = new BABYLON.Color3(0.9, 0.9, 0.9);
+        lumiere.intensity = 0.7;
 
         scene.actionManager.registerAction(
             new BABYLON.ExecuteCodeAction(
                 { trigger: BABYLON.ActionManager.OnKeyUpTrigger, parameter: " " },
-                () => light.setEnabled(!light.isEnabled())
+                () => lumiere.setEnabled(!lumiere.isEnabled())
             )
         );
 
